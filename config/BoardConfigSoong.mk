3b7cd3e4f64c735ec92a92407d8f7d485b112dcd
Alexander Koskovich <zvnexus@outlook.com>
2020-06-28 22:24:53 -0400

    soong: Add TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS. Change-Id: Ibb4202eae237cc9f167707c45dd86a313d95d7be
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -39,6 +39,7 @@ SOONG_CONFIG_conquerGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_fod_lib \
+    target_uses_prebuilt_dynamic_partitions \
     uses_camera_parameter_lib
 
 SOONG_CONFIG_NAMESPACES += conquerNvidiaVars
@@ -68,6 +69,7 @@ SOONG_CONFIG_conquerGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_PO
 SOONG_CONFIG_conquerGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_conquerGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_conquerGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
+SOONG_CONFIG_conquerGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 SOONG_CONFIG_conquerNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_conquerQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_conquerQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
