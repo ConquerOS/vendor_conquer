f8805219dcc8b0ad3aa23fe545370e15297ab2ee
Jake Weinstein <jake@aospa.co>
2020-12-05 22:32:03 +0530

    vendor: import sdclang configs

--- config/common.mk
+++ config/common.mk
@@ -105,6 +105,19 @@ PRODUCT_COPY_FILES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/conquer/overlay/dictionaries
 
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+
+# include definitions for SDCLANG
+include vendor/conquer/sdclang/sdclang.mk
+endif
+
 # Include AOSP audio files
 include vendor/conquer/config/aosp_audio.mk
 
---
+++ sdclang/vendorsetup.sh
@@ -0,0 +1,2 @@
+# SDClang Environment Variables
+export SDCLANG_CONFIG=$(pwd)/vendor/conquer/sdclang/sdclang.json
