f03cada839959bdcf9286aac6eceac56049872fb
Han Wang <416810799@qq.com>
2021-01-16 15:02:35 +0100

    config: props: Remove persist.sys.dun.override again
     * It was removed in 2019, but somehow commit 8926990
       brought it back again.
    
    Change-Id: I973c03f7eed87c46973b42fd7bda04ed1ad27935

--- config/props.mk
+++ config/props.mk
@@ -18,11 +18,6 @@ else
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# Thank you, please drive thru!
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.dun.override=0
-endif
-
 # A/B backuptool
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
