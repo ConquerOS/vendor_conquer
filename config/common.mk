986ee04441e192fbf8f1b0bb08a616b5ef3b2f82
Michael Bestas <mkbestas@gmail.com>
2021-06-14 15:04:40 +0300

    Exclude some files from artifact path requirements
    Change-Id: Id5ef2a44c0cd3b76e3285c26826031f773b7a95f

--- config/common.mk
+++ config/common.mk
@@ -20,11 +20,19 @@ PRODUCT_COPY_FILES += \
     vendor/conquer/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/conquer/prebuilt/common/bin/50-conquer.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-conquer.sh
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/addon.d/50-conquer.sh
+
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
     vendor/conquer/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
     vendor/conquer/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
     vendor/conquer/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/backuptool_ab.sh \
+    system/bin/backuptool_ab.functions \
+    system/bin/backuptool_postinstall.sh
 endif
 
 # Backup Services whitelist
