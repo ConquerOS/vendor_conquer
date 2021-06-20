f82234304e3117458ab2dafca9c4cd2a3fa9278e
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-06-20 09:16:07 +0530

    soong: display headers inclusion workaround for old HALs
    set TARGET_USES_OLD_DISPLAY_HAL := true to include qssi headers fromd device/qcom/common instead from display HAL
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -56,7 +56,8 @@ SOONG_CONFIG_conquerQcomVars += \
     supports_hw_fde_perf \
     uses_pre_uplink_features_netmgrd \
     uses_qcom_bsp_legacy \
-    uses_qti_camera_device
+    uses_qti_camera_device \
+    uses_qcom_old_display_hal
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -80,6 +81,7 @@ SOONG_CONFIG_conquerQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION
 SOONG_CONFIG_conquerQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_conquerQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_conquerQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+SOONG_CONFIG_conquerQcomVars_uses_qcom_old_display_hal := $(TARGET_USES_OLD_DISPLAY_HAL)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
