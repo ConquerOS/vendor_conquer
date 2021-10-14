90d0d5b735eeb8b81973065c777e1816a4aa90b4
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-10-14 14:47:04 +0530

    config: drop blurs
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- config/props.mk
+++ config/props.mk
@@ -27,18 +27,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
 endif
 
-# Blurs
-ifneq ($(TARGET_DISABLE_BLUR), true)
-# Enable blurs, hidden under dev option
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1 \
-    persist.sys.sf.disable_blurs=1
-
-# Hint expensive blurs for device with larger display
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1
-endif
-
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
